977c7a66eb8ff73f128dae9e16e2736b9ec6df99
Ela Yudhanira <ela.yudhanira@scania.com>
2019-12-03 14:19:35 +0100

    Add configuration for symlink
    - Make it possible to work with symlink in vue

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  publicPath: 'corporate-ui-vue'
+  publicPath: 'corporate-ui-vue',
+  // this is used to make npm link works
+  chainWebpack: config => config.resolve.symlinks(false) 
 }
