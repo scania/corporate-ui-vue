d1eb3bc7955c83814065cb30dc7659f955c95139
Marcus Mena <marcus.mena.pacheco@scania.com>
2020-09-18 13:14:31 +0200

    fix small typo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <section>
     <div class="container">
       <h1>Home</h1>
-      <h3>Visit Scania Digtal Design System at <a target='_blank' href='https://digitaldesign.scania.com'>digitaldesign.scania.com</a><c-icon name='scania-external_link'></c-icon></h3>
+      <h3>Visit Scania Digital Design System at <a target='_blank' href='https://digitaldesign.scania.com'>digitaldesign.scania.com</a><c-icon name='scania-external_link'></c-icon></h3>
       <p>Developing in our design system visit our <a target='_blank' href='https://github.com/scania/corporate-ui'>github</a><c-icon name='scania-external_link'></c-icon></p>
     </div>
   </section>
